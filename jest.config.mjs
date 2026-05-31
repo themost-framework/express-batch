d1b10eb0de3031c89f2ff6a24c502770e0b0184c
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2026-05-31 16:07:13 +0000

    bump @themost/express

--- jest.config.mjs
+++ jest.config.mjs
@@ -90,7 +90,7 @@ const config = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    "^@themost/express-batch": "<rootDir>/src/index",
+    "@themost/express-batch": "<rootDir>/src/index"
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
